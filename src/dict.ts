ebac2f5f8a649aebc644d218830d65c664c96391
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-11-07 12:56:27 +0800

    added test cases for dict.ts

--- src/dict.ts
+++ src/dict.ts
@@ -3,22 +3,27 @@ import { Int, ObjectDict } from './types'
 
 export class Dict<K extends string, T extends object> {
   private id: Int | string
+
   constructor(
     private collection: Collection<Record<K, ObjectDict<T>>>,
     private name: K,
     initialValue: T,
   ) {
     const dict = collection.data[name]
-    const entries = Object.entries(dict)
-    if (entries.length > 0) {
-      this.id = entries[0][0]
-    } else {
-      this.id = collection.add(name, initialValue)
+    if (dict) {
+      const entry = Object.entries(dict)[0]
+      if (entry) {
+        this.id = entry[0]
+        return
+      }
     }
+    this.id = collection.add(name, initialValue)
   }
+
   get(): T {
     return this.collection.data[this.name][this.id as number]
   }
+
   update(partialValue: Partial<T>) {
     this.collection.update(this.name, this.id as Int, partialValue)
   }
--- test/dict.spec.ts
+++ test/dict.spec.ts
@@ -2,14 +2,48 @@ import { Dict } from './../src/dict'
 import DB from 'better-sqlite3-helper'
 import { join } from 'path'
 import { Collection } from '../src/collection'
+import { Store } from '../src/store'
+import { ObjectDict } from '../src/types'
+import { existsSync, unlinkSync } from 'fs'
+import { expect } from 'chai'
 
 describe('Dict TestSuit', () => {
-  let dict: Dict<>
+  type Config = {
+    origin: string
+    pathname: string
+    version: string
+  }
+  let collection: Collection<{ config: ObjectDict<Config> }>
+  let dict: Dict<'config', Config>
   before(() => {
     let dbFile = join('data', 'dict-test.db')
+    if (existsSync(dbFile)) {
+      unlinkSync(dbFile)
+    }
     let db = DB({ path: dbFile, migrate: false })
-    let collection = new Collection(db)
-    dict = new Dict()
+    let store = new Store(db)
+    collection = new Collection<{ config: ObjectDict<Config> }>(store)
   })
-  it('should initialize dict', () => {})
+  it('should initialize dict', () => {
+    dict = new Dict<'config', Config>(collection, 'config', {
+      origin: 'http://localhost:3000',
+      pathname: '/api',
+      version: '1.0.0',
+    })
+  })
+  it('should load dict value', () => {
+    expect(dict.get()).deep.equals({
+      origin: 'http://localhost:3000',
+      pathname: '/api',
+      version: '1.0.0',
+    })
+  })
+  it('update update dict', () => {
+    dict.update({ version: '1.0.1' })
+    expect(dict.get()).deep.equals({
+      origin: 'http://localhost:3000',
+      pathname: '/api',
+      version: '1.0.1',
+    })
+  })
 })
